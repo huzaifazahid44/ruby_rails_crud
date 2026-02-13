a66da28c0c6fbc98b672d865e3dfe987f6fb1a8c
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-13 14:06:42 +0500

    Added a new Users table.
    created users table with fields name and email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_11_115926) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_13_085815) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
