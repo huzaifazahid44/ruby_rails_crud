12949ca8cc3ec8a93986db8d57a065804dc51301
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-13 16:10:34 +0500

    Added Secure Password
    Using Bcrypt added secure password for each user which is saved in password_digest.
    has_secure_password ensures password is hashed and then saved in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_13_092728) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_13_105633) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_13_092728) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 end
