4ec5ddebec10bee586594581a5d3f6110d810918
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-16 14:18:09 +0500

    Helper methods available to controllers
    added the applications_helper methods that are available to all controllers and the views (through helper_method).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  helper_method :logged_in?
+  def logged_in?
+    current_user.present?
+  end
+
 end
