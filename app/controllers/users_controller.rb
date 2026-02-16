4073cab9403693249df263a4e9e275c5f2a255f3
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-16 15:31:35 +0500

    User Delete Profile Option
    added user option to delete their profile, deleting user also deletes all the articles linked to the user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :edit, :update]
+before_action :set_user, only: [:show, :edit, :update, :destroy]
 before_action :require_user, only: [:edit, :update]
-before_action :require_same_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -32,6 +32,13 @@ before_action :require_same_user, only: [:edit, :update]
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted."
+    redirect_to root_path
   end
 
   private
