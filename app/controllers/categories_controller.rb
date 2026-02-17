7073490d54d580e1b34c6246f85ebe2b360ce588
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-17 17:29:30 +0500

    Categories templates and methods
    added categories controllers and views(
    partials forms for editing or creating categories) and more

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-    before_action :require_admin, only: [:new, :create]
+    before_action :require_admin, only: [:new, :create, :edit ,:update]
 
     def new
         @category = Category.new
@@ -21,6 +21,21 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category was successfully updated."
+            redirect_to category_path(@category)
+        else
+            render :edit
+        end
     end
 
     private
